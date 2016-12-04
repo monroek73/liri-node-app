be6bb59e9c70057d71f21c92924444f1e1cf2b0a
Kim <monroek73@yahoo.com>
2016-12-04 11:34:35 -0800

    a

--- keys.js/keys.js
+++ keys.js/keys.js
@@ -1,8 +1,13 @@
 console.log('this is loaded');
 
 exports.twitterKeys = {
-  consumer_key: '<input here>',
-  consumer_secret: '<input here>',
-  access_token_key: '<input here>',
-  access_token_secret: '<input here>',
+  consumer_key: 'cANMXMokPtDt0P83kqPAFpY41',
+  consumer_secret: 'tUVCapwxwaxHx6I56rSgsYLMNftiTIvG6Z6RPiEiqsS8H959fQ',
+  access_token_key: '35086467-1ItNfyBBkls5vV42J4spEDiYxB1R32C7zad3GuV0y',
+  access_token_secret: 'RKkUuMDVHiGQr5gQuLBsCrDqv18WwZKy7ABXaYkQkgHFJ',
+}
+
+exports.spotify= {
+client_id:'9b8eb591fae747bb8ac055fa0c499622',
+client_secret: 'f89258ae2c3740ba82d567e7ed61ad90',
 }
